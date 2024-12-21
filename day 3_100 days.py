81754d2774a1c2befd9099c44432a471eeff1013
rahatgulshan <rahat.gulshan@gmail.com>
2024-12-21 15:15:42 +0500

    My first commit on my Third Day at Python Coding.

---
+++ day 3_100 days.py
@@ -0,0 +1,8 @@
+print("MENU")
+print("")
+strFood=input("Name a food > ")
+strPlant=input("Name a type of plant > ")
+strCooking=input("Name a method of cooking > ")
+strBurnedWord=input("What word describes burned food? > ")
+strDiyItem=input("Name a DIY item > ")
+print(strCooking, strFood, "with ", strBurnedWord, strPlant, "on a bed of ", strDiyItem)

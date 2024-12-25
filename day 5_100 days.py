c29df71ecd2f607302061a1cabd3e3ec901faf8f
rahatgulshan <rahat.gulshan@gmail.com>
2024-12-25 12:19:05 +0500

    Fourth Day

---
+++ day 5_100 days.py
@@ -0,0 +1,30 @@
+# strmyName=input("What's your name?:  ")
+# if strmyName == "David":
+#    print("You are just the baldest person I've ever seen")
+# else:
+#    print("Who on earth are you?!")
+# _______________________________
+# strCatsOrDogs=input("Are you a cat person or a dog? ")
+# if strCatsOrDogs=="cats":
+#    print("Meown")
+# else:
+#    print("Woof")
+# __________________________________
+print("Marvel Movie Character Creater")
+print("--")
+print(" ")
+strSpiderMan=input("Do you like 'hanging around'?: ")
+if strSpiderMan=="No":
+   print("Then you're not Spider-Man")
+else:
+   print("Then you're Spider-Man")
+strGravelyVoice=input("Do you've a 'Gravely Voice'? ")
+if strGravelyVoice=="No":
+   print("You're not korg")
+else:
+   print("You're korrg")
+strCaptainMarvel=input("Do you often feel Marvelous? ")
+if strCaptainMarvel=="Yes":
+   print("Aha! You're Captain Marvel! Hi!")
+else:
+   print("You're not Captain Marvel")

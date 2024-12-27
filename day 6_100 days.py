6cead3594b45be3b70358ed738c2c0b65efe7d25
rahatgulshan <rahat.gulshan@gmail.com>
2024-12-27 15:13:05 +0500

    Sixth Day at Python

---
+++ day 6_100 days.py
@@ -0,0 +1,27 @@
+# print("SECURE PASSWORD")
+# strUserName=input("UserName >: ")
+# strpassword=input("Password >: ")
+# if strUserName=="mark" and strpassword=="password":
+#    print("Welcome Mark!")
+# elif strUserName=="suzanne" and strpassword=="Su74nne":
+#    print("Hey there suzanne!")
+# else:
+#    print("Go away")
+# ___________________________________________
+
+print ("MY LOGIN SYSTEM")
+print("+++++++++++++++++")
+strUserName=input("Username > ")
+strPassword=input("Password > ")
+if strUserName=="David" and strPassword=="totallyNotBald":
+   print(" ")
+   print("Why hello there", strUserName," What a lovely accent you have, You could have charmed your way in here even without a password.")
+   print(" ")
+   print("Have a great day.")
+   print("Don't forget to wear a hat in the sun")
+elif strUserName=="William" and strPassword=="123456":
+   print ("Hi" , strUserName, "How are you?")
+elif strUserName=="Sara" and strPassword=="147258":
+   print("Hello ", strUserName)
+else:
+   print("You don't have access to this page.")

484d4c5f1d98a1a6138807d0685e07fac482df26
rahatgulshan <rahat.gulshan@gmail.com>
2024-12-29 14:45:26 +0500

    Seventh Day coding in Python

---
+++ day 7_100 days.py
@@ -0,0 +1,33 @@
+# tvShow = input("What is your favorite tv show? ")
+# if tvShow == "Sarah & Duck":
+#   print("Ugh, why?")
+#   faveCharacter = input("Who is your favorite character? ")
+#   if faveCharacter == "Duck":
+#     print("Right answer")
+#   else:
+#     print("Nah, Sarah is the greatest")
+# elif tvShow == "Tom & Jerry":
+#   print("Aha, Funny Times")
+# else:
+#   print("Yeah, that's cool and all…")
+# ____________________________________________
+
+print("🧐 Fake Fan Finder 👀")
+print("----------------------")
+
+strFavAnime=input("What's your favourite Anime? ")
+if strFavAnime=="one piece":
+   print("oh really!")
+   strChar=input("Name me any of the characters? ")
+   if strChar=="Naml":
+      print("You got that be pure chance. Okay then")
+   strJob=input("What's her job on the ship? ")
+   if strJob=="Navigator":
+      print("Humph")
+   strBounty= input("What was her first bounty then?")
+   if strBounty=="Ummm..":
+      print("See!, Fake one piece fan!")
+else:
+   print("OHHH")
+
+

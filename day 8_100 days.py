b00e215dcf86a0812e2f437815aa0366aaf6ae0e
rahatgulshan <rahat.gulshan@gmail.com>
2025-01-01 15:40:24 +0500

    Eighth Day of Python Coding.

---
+++ day 8_100 days.py
@@ -0,0 +1,41 @@
+# print("Wholesome Positivity Machine")
+# strName=input("Who are you? ")
+# if strName=="David" or strName=="david":
+#       print(strName)
+# strGoal=input("What do you want to achieve? ")
+# print(strGoal)
+# intRating=input("On the scale of 1 - 10 how do you feel today? (1:😥, 10:🥳) ")
+# print("Hey",strName, "Cheerup! Today you want to ",strGoal,"in the way simply by being you - YOU ROCK!")
+# else:
+# print(" ")
+# ____________________________________
+
+print("Hello, Welcome to your daily afirmation generator." )
+strName=input("What is your name? ")
+if strName=="Mark" or strName=="mark":
+   strDOW=input("What is the day of the week? ")
+   if strDOW=="Monday" or strDOW=="monday":
+      print("Its going to be a great Monday",strName)
+   if strDOW=="Tuesday" or strDOW=="tuesday":
+      print("What a wondeful tuesday it is",strName)
+   if strDOW=="Wednesday" or strDOW=="wednesday":
+      print("Happy Hump Day",strName)
+   if strDOW=="Thursday" or strDOW=="thursday":
+      print(strName, "Your week is almost over")
+   if strDOW=="Friday" or strDOW=="friday":
+      print(strName,"Its FRIDAY!")
+
+elif strName=="David" or strName=="david":
+   strDOW=input("What is the day of the week? ")
+   if strDOW=="Monday" or strDOW=="monday":
+      print("You looks great today",strName)
+   if strDOW=="Tuesday" or strDOW=="tuesday":
+      print("Have a nice tuesday",strName)
+   if strDOW=="Wednesday" or strDOW=="wednesday":
+      print("Hump Day! Finally",strName)
+   if strDOW=="Thursday" or strDOW=="thursday":
+      print(strName, "What a fantastic day")
+   if strDOW=="Friday" or strDOW=="friday":
+      print(strName,"Its FRIDAY!")
+else:
+   print("I don't know your name, but have a great day! ")

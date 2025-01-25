8c7e8b0177d9a49da6213980fd11bf9b590f9709
rahatgulshan <rahat.gulshan@gmail.com>
2025-01-25 16:23:17 +0500

    16th Paython Codind Day

---
+++ day 16_100 days.py
@@ -0,0 +1,47 @@
+# counter = 0
+# while True:
+#   answer = int(input("Enter a number: "))
+#   print("Adding it up!")
+#   counter += answer
+#   print("Current total is", counter)
+#   addAnother = input("Add another? ")
+#   if addAnother == "no":
+#     break
+# print("Bye!")
+
+# __________________________
+
+# counter = 0
+# while True:
+#   answer = int(input("Enter a number: "))
+#   print("Adding it up!")
+#   counter += answer
+#   print("Current total is", counter)
+#   addAnother = input("Add another? ")
+#   if addAnother == "no":
+#    break
+# print("Bye!")
+# ___________________________________
+
+# while True:
+#   color = input("Enter a color: ")
+#   if color == "red":
+#    break
+#   else:
+#     print("Cool color!")
+# print("I don't like red")
+
+# ___________________________________
+counter=0
+print("Fill in the blank lyrics!")
+print()
+print("\(Type in the blank lyrics and see if you are as cool as me.)")
+print()
+while True:
+   word=input("Never going to ______ you up.\n")
+   counter += 1
+   if word=="give" or word=="Give":
+      print("Well done! It only took you",counter,"attempts.")
+      break
+   else:
+      print("Nope, try again.")
